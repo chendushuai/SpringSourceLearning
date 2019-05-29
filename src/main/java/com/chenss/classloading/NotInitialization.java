169b3d9f47e84b1f98353a69dee2b1e22f38c3bc
chendushuai <chendushuai@126.com>
2019-05-29 18:31:05 +0800

    类加载机制-被动使用类字段演示三 常量在编译阶段会存入调用类的常量池中，本质上并没有直接引用到定义常量的类，因此不会触发定义常量的类的初始化。

--- src/main/java/com/chenss/classloading/NotInitialization.java
+++ src/main/java/com/chenss/classloading/NotInitialization.java
@@ -1,13 +1,10 @@
 package com.chenss.classloading;
 
 /**
- * 被动使用类字段演示二
- * 通过数组定义来引用类，不会触发此类的初始化
+ * 非主动使用类字段演示
  */
 public class NotInitialization {
     public static void main(String[] args) {
-        //System.out.println(SubClass.value);
-        SuperClass[] sca = new SuperClass[10];
-        System.out.println(sca.getClass());
+        System.out.println(ConstClass.HELLOWORLD);
     }
 }
