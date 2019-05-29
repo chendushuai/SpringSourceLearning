aec8eb957eb37032cc0421b93e2b9aed61b18285
chendushuai <chendushuai@126.com>
2019-05-29 18:15:07 +0800

    类加载机制-被动使用类字段演示二

--- src/main/java/com/chenss/classloading/NotInitialization.java
+++ src/main/java/com/chenss/classloading/NotInitialization.java
@@ -1,12 +1,13 @@
 package com.chenss.classloading;
 
 /**
- * 非主动使用类字段演示
+ * 被动使用类字段演示二
+ * 通过数组定义来引用类，不会触发此类的初始化
  */
 public class NotInitialization {
     public static void main(String[] args) {
-        System.out.println(SubClass.value);
-        //SuperClass[] sca = new SuperClass[10];
-        //System.out.println(sca.getClass());
+        //System.out.println(SubClass.value);
+        SuperClass[] sca = new SuperClass[10];
+        System.out.println(sca.getClass());
     }
 }
