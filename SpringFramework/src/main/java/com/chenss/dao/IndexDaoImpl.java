6d2bd66f5774de331eb24f367ab767c74dba577c
chendushuai <chendushuai@126.com>
2019-06-01 07:54:46 +0800

    增加Spring框架学习项目 注入字符串属性值

--- SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl.java
+++ SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl.java
@@ -1,8 +1,17 @@
 package com.chenss.dao;
 
 public class IndexDaoImpl implements IndexDao {
+    private String str;
     @Override
     public void test() {
-        System.out.println("impl");
+        System.out.println("impl " + str);
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
     }
 }
