729796e96dd31357408485a71388f6cb4c8f04bf
chendushuai <chendushuai@126.com>
2019-06-01 09:26:10 +0800

    根据类型默认自动装配，使用default-autowire指定默认装配类型

--- SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl.java
+++ SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl.java
@@ -3,17 +3,8 @@ package com.chenss.dao;
 import org.springframework.stereotype.Component;
 
 public class IndexDaoImpl implements IndexDao {
-    private String str;
     @Override
     public void test() {
-        System.out.println("impl " + str);
-    }
-
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
+        System.out.println("impl ");
     }
 }
--- SpringFramework/src/main/java/com/chenss/dao/IndexService.java
+++ SpringFramework/src/main/java/com/chenss/dao/IndexService.java
@@ -3,16 +3,13 @@ package com.chenss.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 public class IndexService {
     //也需要描述关系
-    @Autowired
     private IndexDao dao;
 
-    public IndexService(IndexDao dao) {
+    public void setDao(IndexDao dao) {
         this.dao = dao;
     }
-
 
     public void service() {
         dao.test();
