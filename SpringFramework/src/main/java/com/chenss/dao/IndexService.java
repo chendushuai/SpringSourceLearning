af17c5f75d21fb5d2378a2492fde03c8aab838b6
chendushuai <chendushuai@126.com>
2019-06-02 07:28:13 +0800

    使用@Lookup注解实现在单例模型中注入原型模式对象

--- SpringFramework/src/main/java/com/chenss/dao/IndexService.java
+++ SpringFramework/src/main/java/com/chenss/dao/IndexService.java
@@ -1,9 +1,7 @@
 package com.chenss.dao;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +10,8 @@ import javax.annotation.Resource;
 @Service
 @Scope("singleton")//默认是单例的
 //@Scope("prototype")
-public class IndexService implements ApplicationContextAware {
+public class IndexService {
     //也需要描述关系
-    @Autowired//默认使用byType注入形式
     private IndexDao dao;
 
     /*@Resource//默认使用byName进行处理，可能是根据参数名称进行匹配类名
@@ -24,14 +21,14 @@ public class IndexService implements ApplicationContextAware {
         this.dao = dao;
     }
 
-    public void service() {
-        IndexDao dao = (IndexDao) this.applicationContext.getBean("dao");
-        System.out.println("dao -- " + dao.hashCode());
+    @Lookup
+    public IndexDao getDao() {
+        return null;
     }
-    private ApplicationContext applicationContext;
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext=applicationContext;
+    public void service() {
+        getDao().test();
+        System.out.println("dao == " + getDao().hashCode());
+        System.out.println("service == " + this.hashCode());
     }
 }
