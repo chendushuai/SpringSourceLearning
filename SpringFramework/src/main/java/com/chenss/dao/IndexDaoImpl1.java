5da6a8ae021e2d389ce8b775e7a8c84456f04f8d
chendushuai <chendushuai@126.com>
2019-06-02 06:58:36 +0800

    区分使用@Resource和@Autowired注解，@R默认根据name，@A默认根据type

--- SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl1.java
+++ SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl1.java
@@ -1,5 +1,8 @@
 package com.chenss.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class IndexDaoImpl1 implements IndexDao {
     @Override
     public void test() {
--- SpringFramework/src/main/java/com/chenss/dao/IndexService.java
+++ SpringFramework/src/main/java/com/chenss/dao/IndexService.java
@@ -3,15 +3,21 @@ package com.chenss.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 public class IndexService {
     //也需要描述关系
-    private IndexDao dao;
+    /*@Autowired//默认使用byType注入形式
+    private IndexDao dao;*/
 
-    public void setDao(IndexDao dao) {
-        this.dao = dao;
+    @Resource//默认使用byName进行处理，可能是根据参数名称进行匹配类名
+    private IndexDao indexDaoImpl1;
+
+    public void setDaowessss(IndexDao dao) {
+        this.indexDaoImpl1 = dao;
     }
 
     public void service() {
-        dao.test();
+        indexDaoImpl1.test();
     }
 }
