d7b66da40956bd3f9e692eba6be22cab1ac595c1
chendushuai <chendushuai@126.com>
2019-06-02 10:46:10 +0800

    在 @ComponentScan扫描时，使用 @ComponentScan.Filter排除掉指定包中的文件扫描，或直接排除掉指定类型注解的扫描

--- SpringFramework/src/main/java/com/chenss/config/Spring.java
+++ SpringFramework/src/main/java/com/chenss/config/Spring.java
@@ -1,11 +1,16 @@
 package com.chenss.config;
 
+import com.chenss.service.IndexService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Service;
 
 @Configuration
-@ComponentScan("com.chenss")
+//使用下面一行的形式在扫描Bean时排除掉指定包中的类，或使用excludeFilters = @ComponentScan.Filter(Service.class)排除掉指定注解类型的类
+@ComponentScan(value = "com.chenss", excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.chenss.service.*"))
+//@ComponentScan(value = "com.chenss", excludeFilters = @ComponentScan.Filter(Service.class))
 @ImportResource("classpath:spring.xml")//三者混合使用，导入XML配置文件
 public class Spring {
 }
