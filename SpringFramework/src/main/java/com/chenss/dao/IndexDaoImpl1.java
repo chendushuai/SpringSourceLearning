c242759f7f65125f6c229e48435fdaf2ec17faa5
chendushuai <chendushuai@126.com>
2019-06-02 09:17:58 +0800

    使用depends-on要求依赖的内容先加载

--- SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl1.java
+++ SpringFramework/src/main/java/com/chenss/dao/IndexDaoImpl1.java
@@ -3,6 +3,10 @@ package com.chenss.dao;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import javax.annotation.PostConstruct;
+
 public class IndexDaoImpl1 {
+    public IndexDaoImpl1() {
+        System.out.println("IndexDaoImpl1 init");
+    }
 }
