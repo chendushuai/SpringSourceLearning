d5874e2dc3d9e1e25fb496450c584865e05443a0
chendushuai <chendushuai@126.com>
2019-06-03 01:01:25 +0800

    配合Mybatis实现数据库连接配置，放置在JavaConfig文件中实现，注意使用的@Bean标签

--- SpringFramework/src/main/java/com/chenss/config/Spring.java
+++ SpringFramework/src/main/java/com/chenss/config/Spring.java
@@ -1,14 +1,34 @@
 package com.chenss.config;
 
 import com.chenss.service.IndexService;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.ImportResource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.chenss")
 @ImportResource("classpath:spring.xml")//三者混合使用，导入XML配置文件
 public class Spring {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setPassword("root");
+        driverManagerDataSource.setUrl("root");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/");
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return driverManagerDataSource;
+    }
+    @Bean
+    @Autowired//Spring5之后可以不写
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        System.out.println("chenss002");
+        return sqlSessionFactoryBean;
+    }
 }
