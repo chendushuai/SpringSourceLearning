41c93c4a8f80e0363dc5b10cde228c3301c82a06
chendushuai <chendushuai@126.com>
2019-09-05 23:44:39 +0800

    合并两个有序链表。直接使用第一个元素为-1 的方式，循环判断两个链表，进行合并

---
+++ SpringFramework/src/main/java/com/chenss/algorithm/MergeTwoLists.java
@@ -0,0 +1,47 @@
+package com.chenss.algorithm;
+
+import com.alibaba.fastjson.JSONObject;
+import jdk.nashorn.internal.ir.IfNode;
+
+public class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (null == l1) {
+            return l2;
+        } else if (null == l2) {
+            return l1;
+        }
+        ListNode result = new ListNode(-1);
+        ListNode curr = result;
+        while (null != l1 && null != l2) {
+            if (l1.val<l2.val) {
+                curr.next=l1;
+                l1=l1.next;
+            } else {
+                curr.next=l2;
+                l2=l2.next;
+            }
+            curr=curr.next;
+        }
+        if (null == l1) {
+            curr.next=l2;
+        } else if (null ==l2) {
+            curr.next=l1;
+        }
+        return result.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next=new ListNode(2);
+        l1.next.next=new ListNode(4);
+        ListNode l2 = new ListNode(2);
+        l2.next=new ListNode(3);
+        l2.next.next=new ListNode(4);
+        MergeTwoLists lis = new MergeTwoLists();
+        ListNode listNode = lis.mergeTwoLists(l1,l2);
+        while (null != listNode.next) {
+            System.out.println(listNode.val);
+            listNode=listNode.next;
+        }
+    }
+}
