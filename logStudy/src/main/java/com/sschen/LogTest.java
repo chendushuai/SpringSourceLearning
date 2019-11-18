e9559724541f5d124f873ce4109ff6a9423f2290
chendushuai <chendushuai@126.com>
2019-11-18 16:31:19 +0800

    logback 打印日志工厂状态信息

--- logStudy/src/main/java/com/sschen/LogTest.java
+++ logStudy/src/main/java/com/sschen/LogTest.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogTest {
+    /**
+     * 如果给定的全限定名称一样，则获取的日志对象是一个
+     */
     private static Logger logger = LoggerFactory.getLogger(LogTest.class);
     public static void main(String[] args) {
         logger.info("info log");
