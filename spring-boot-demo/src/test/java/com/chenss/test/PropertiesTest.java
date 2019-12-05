9967486743a52c965c3945a0d70c7f5fe3ed06d5
chendushuai <chendushuai@126.com>
2019-12-05 21:38:36 +0800

    读取YML文件自动注入

--- spring-boot-demo/src/test/java/com/chenss/test/PropertiesTest.java
+++ spring-boot-demo/src/test/java/com/chenss/test/PropertiesTest.java
@@ -4,6 +4,7 @@ import com.chenss.properties.Student;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,8 +14,15 @@ public class PropertiesTest {
     @Autowired
     Student student;
 
+    @Value("${name}")
+    String name;
+    @Value("${age}")
+    int age;
+
     @Test
     public void readTest(){
         System.out.println(student);
+
+        System.out.println("name= " + name + "   age:   " + age);
     }
 }
