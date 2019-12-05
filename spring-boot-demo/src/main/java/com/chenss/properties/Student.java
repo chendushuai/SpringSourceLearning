9203a14121d0474f5e96af8c9511fdc04c02bb7a
chendushuai <chendushuai@126.com>
2019-12-05 21:08:53 +0800

    读取YML文件自动注入

---
+++ spring-boot-demo/src/main/java/com/chenss/properties/Student.java
@@ -0,0 +1,45 @@
+package com.chenss.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "student")
+public class Student {
+    private String name;
+    private int age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
