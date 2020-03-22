968d24f1386d87a8fc2e9226848e280f2c2adc54
chendushuai <chendushuai@126.com>
2020-03-22 23:55:23 +0800

    合并区间

--- SpringFramework/src/main/java/com/chenss/algorithm/Merge.java
+++ SpringFramework/src/main/java/com/chenss/algorithm/Merge.java
@@ -9,47 +9,46 @@ import java.util.List;
 
 /**
  * 56. 合并区间
- *
+ * <p>
  * 给出一个区间的集合，请合并所有重叠的区间。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: [[1,3],[2,6],[8,10],[15,18]]
  * 输出: [[1,6],[8,10],[15,18]]
  * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
  * 示例 2:
- *
+ * <p>
  * 输入: [[1,4],[4,5]]
  * 输出: [[1,5]]
  * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
  */
 public class Merge {
     public static int[][] merge(int[][] intervals) {
-        if (intervals.length<=1) {
+        if (intervals.length <= 1) {
             return intervals;
         }
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[0]-o2[0];
+                return o1[0] - o2[0];
             }
         });
 
         int last = 0;
         for (int i = 1; i < intervals.length; i++) {
             int[] nex = intervals[i];
-            if (nex[0]<=intervals[last][1]) {
-
-                intervals[last][1]=Math.max(intervals[last][1],nex[1]);
+            if (nex[0] <= intervals[last][1]) {
+                intervals[last][1] = Math.max(intervals[last][1], nex[1]);
             } else {
-                intervals[++last]=nex;
+                intervals[++last] = nex;
             }
         }
-        return Arrays.copyOf(intervals,last+1);
+        return Arrays.copyOf(intervals, last + 1);
     }
 
     public static void main(String[] args) {
         Gson gson = new Gson();
-            System.out.println(gson.toJson(merge(new int[][]{{1,3},{2,6},{8,10},{15,18}})));
+        System.out.println(gson.toJson(merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}})));
     }
 }
